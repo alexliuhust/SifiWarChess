930affe08b8024e13c24eba8e472e4a3f2b2fde3
Zehua(Alexander) Liu <68038532+alexliuhust@users.noreply.github.com>
2021-01-06 09:19:00 -0500

    Adjust some continuous skills' period

--- SiFiWarChess/src/com/sifi/test/Test.java
+++ SiFiWarChess/src/com/sifi/test/Test.java
@@ -8,31 +8,31 @@ import com.sifi.model.Unit;
 public class Test {
 	
 	public static void attackTest0() {
-		Unit Marine = UnitGet.getUnitByName("Marine");
+		Unit Storm = UnitGet.getUnitByName("Storm Chariot");
 		Unit Battleship = UnitGet.getUnitByName("Battleship");
+		Battleship.shield = 200;
+		Battleship.c_shield = 200;
 		
-		Battleship.cast("Volley",Battleship);
-		Battleship.cast("Space jump",Battleship);
-		
-		Marine.cast("Shield Defense", Marine);
 		System.out.println(Battleship);
-		System.out.println(Marine);
+		System.out.println(Storm);
 		
 		System.out.println("=================1=================");
-		AttTrigger.attackTigger(Marine, Battleship);
-		System.out.println(Marine);
-		
-		Battleship.myTurnStart();
-		Marine.myTurnStart();
-		Battleship.myTurnStart();
-		Marine.myTurnStart();
-		Battleship.myTurnStart();
-		Marine.myTurnStart();
-		Battleship.myTurnStart();
-		Marine.myTurnStart();
-		System.out.println("=================5=================");
+		Storm.cast("EMP", Battleship);
 		System.out.println(Battleship);
-		System.out.println(Marine);
+		System.out.println(Storm);
+		
+		System.out.println("=================2=================");
+		Battleship.myTurnStart();
+		Storm.myTurnStart();
+		Storm.cast("EMP", Battleship);
+		System.out.println(Battleship);
+		System.out.println(Storm);
+		
+		System.out.println("=================3=================");
+		Battleship.myTurnStart();
+		Storm.myTurnStart();
+		System.out.println(Battleship);
+		System.out.println(Storm);
 		
 		
 		
