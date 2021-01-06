a787b1c9c82b876876bb7511cfd256547a296c93
Zehua(Alexander) Liu <68038532+alexliuhust@users.noreply.github.com>
2021-01-05 23:05:52 -0500

    Test Medican skills

--- SiFiWarChess/src/com/sifi/test/Test.java
+++ SiFiWarChess/src/com/sifi/test/Test.java
@@ -9,23 +9,32 @@ public class Test {
 	
 	public static void attackTest0() {
 		Unit marine1 = UnitGet.getUnitByName("marine");
+		Unit medican = UnitGet.getUnitByName("Medican");
+		marine1.c_scale -= 40;
 		System.out.println(marine1);
+		System.out.println(medican);
 		
 		System.out.println("=================1=================");
-		marine1.skills.get("Adrenaline").trigger(marine1, marine1);
+		medican.skills.get("Medical nanoworm").trigger(medican, marine1);
 		System.out.println(marine1);
+		System.out.println(medican);
 		
 		System.out.println("=================2=================");
 		marine1.myTurnStart();
+		medican.myTurnStart();
+		medican.skills.get("Medical nanoworm").trigger(medican, marine1);
 		System.out.println(marine1);
+		System.out.println(medican);
 		
 		System.out.println("=================3=================");
 		marine1.myTurnStart();
+		medican.myTurnStart();
 		System.out.println(marine1);
-		
-		System.out.println("=================4=================");
-		marine1.myTurnStart();
+		System.out.println(medican);
+		System.out.println("====Emergency====");
+		medican.skills.get("Emergency").trigger(medican, marine1);
 		System.out.println(marine1);
+		System.out.println(medican);
 		
 	}
 	
